4358c06ad328f18cc3212e240423bf093cd0a42a
KEPEL9029 <168377719+KEPEL9029@users.noreply.github.com>
2024-10-26 10:49:21 +0300

    ferst add in git

--- src/main/java/org/example/database/PostgresConnection.java
+++ src/main/java/org/example/database/PostgresConnection.java
@@ -1,8 +1,11 @@
 package org.example.database;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection; //Импортирует интерфейс Connection, который используется для взаимодействия с базой данных.
 import java.sql.DriverManager; //Импортирует класс DriverManager, который управляет набором драйверов для баз данных. Через этот класс осуществляется подключение к базе данных.
 import java.sql.SQLException; //Импортирует исключение SQLException, которое выбрасывается при возникновении проблем с базой данных.
+import java.util.Properties;
 
 public class PostgresConnection {
     private String password;
@@ -11,11 +14,20 @@ public class PostgresConnection {
     private String driver;
 
 
-    public PostgresConnection(){
-        this.password = "postgres";
-        this.userName = "postgres";
-        this.url ="jdbc:postgresql://localhost:5432/shop?currentSchema=public";
-        this.driver="org.postgresql.Driver";
+    public PostgresConnection() {
+        Properties properties = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("database.properties")) {
+            if (input == null) {
+                throw new IOException("Файл database.properties не найден");
+            }
+            properties.load(input);
+            this.url = properties.getProperty("db.url");
+            this.userName = properties.getProperty("db.username");
+            this.password = properties.getProperty("db.password");
+            this.driver = properties.getProperty("db.driver");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
